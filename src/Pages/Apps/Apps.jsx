cf1fa0911462c4ebbb9f89511af9e90556cff58c
Sahidulislam05 <sahidulislamcst@gmail.com>
2025-10-09 18:23:31 +0600

    Added Hero.IO

--- src/Pages/Apps/Apps.jsx
+++ src/Pages/Apps/Apps.jsx
@@ -69,6 +69,8 @@ const Apps = () => {
         </div>
       )} */}
 
+        {/* All Apps Page Data Loading */}
+
         {searchLoading ? (
           <LoadingAnimation></LoadingAnimation>
         ) : searchedApp.length > 0 ? (
